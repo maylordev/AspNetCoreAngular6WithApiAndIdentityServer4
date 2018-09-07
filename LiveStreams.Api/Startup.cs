0e56513d40197bc934b8239873d762fd90a03016
Matt Taylor <maylortaylor@gmail.com>
2018-09-07 19:27:41 -0400

    added features design pattern for MVC and angular

--- LiveStreams.Api/Startup.cs
+++ LiveStreams.Api/Startup.cs
@@ -10,6 +10,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
+using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Net.Http.Headers;
 
 namespace LiveStreams.Api
 {
